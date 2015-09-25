e08861782b715d1dab9bf849ccd0a76ee3a83342
lmilius <lmilius12@gmail.com>
2015-09-24 22:48:57 -0500

    Added scripts to insert and delete users for the dino website.

---
+++ insertUser.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import MySQLdb as mdb
+import ConfigParser
+import logging
+import re
+
+
+SQLINJECTION = re.compile(r'([^a-zA-Z0-9.])')
+FORMAT = "%(asctime)-15s %(levelname)s - %(message)s"
+logging.basicConfig(filename='insertUser.log', level=logging.DEBUG, format=FORMAT)
+
+def connectDB():
+	configSection = 'MySQL'
+	host = None
+	username = None
+	password = None
+	database = None
+	try:
+		config = ConfigParser.ConfigParser()
+		config.read('server.cfg')
+		host = config.get(configSection, 'host')
+		username = config.get(configSection, 'username')
+		password = config.get(configSection, 'password')
+		database = config.get(configSection, 'database')
+	except:
+		logging.warn("Error reading configuration file.")
+		logging.debug('%s %s %s %s' % (host, username, password, database))
+		return None, None
+
+	try:
+		con = mdb.connect(host, username, password, database)
+		cur = con.cursor()
+		logging.debug("Conected to database.")
+		return con, cur
+	except:
+		logging.warn("Connection to database failed.")
+		logging.debug('%s %s %s %s' % (host, username, password, database))
+		return None, None
+
+def closeDB(con):
+	con.commit()
+
+def insertUser(username, password):
+	if SQLINJECTION.search(username):
+		logging.debug('SQL Injection found!')
+		return False
+	con, cur = connectDB()
+	query = "INSERT INTO USER VALUES('%s', '%s');" % (username, password)
+	logging.debug(query)
+	exe = cur.execute(query)
+	if exe == 1:
+		print 'Success!'
+	else:
+		print 'Failed.'
+	closeDB(con)
+
+def main():
+	print 'start'
+	username = raw_input('Enter the new username: ')
+	print username
+	password = raw_input('Enter the new password: ')
+	insertUser(username, password)
+
+if  __name__ =='__main__':
+	main()
